d6aa33f30099f49593ab5a82aef81af0947b6cb4
abunyamin <85947059+abunyamin@users.noreply.github.com>
2023-03-01 20:38:48 +0700

    modified for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-export default {
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
-  base: '/ui-challenge-2/',
-};
+export default defineConfig({
+  plugins: [react()],
+})
