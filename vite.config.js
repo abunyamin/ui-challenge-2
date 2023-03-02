4ab8973f5d57a4b4186b8107f02da6114c01c405
abunyamin <85947059+abunyamin@users.noreply.github.com>
2023-03-02 14:38:30 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   // base: '/ui-challenge-2/', // activated this if you want to deploy to github page, value of this property is depending your respository.
 
   build: {
-    outDir: 'docs', // the results of the build process are going to the docs directory
     assetsDir: 'assets', // This is Asset folder for your image etc.
     assetsInlineLimit: 0,
     sourcemap: false,
